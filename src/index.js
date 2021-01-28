075b9d7f2f1fcb227878ec5bf1c4daf9f71ac44f
John Rojcewicz <rojcewiczj@gmail.com>
2021-01-27 22:13:36 -0600

    exploring reducer/action/store relationship

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import store from './store';
+
+const unsubscribe = store.subscribe(()=> {
+    console.log("Store has changed", store.getState())
+})
+store.dispatch({
+    type: "bugAdded",
+    payload: {
+        description: "Bug1"
+    }
+})
+unsubscribe();
+store.dispatch({
+    type: "bugRemoved",
+    payload: {
+        id: 1
+    }
+})
+console.log(store.getState())
--- src/reducer.js
+++ src/reducer.js
@@ -1,17 +1,18 @@
 
 
 let lastId = 0;
-function reducer(state = [], action){
+export default function reducer(state = [], action){
     if(action.type === "bugAdded"){
     return [
         ...state,
         {   
             id: ++lastId,
-            description: payload.description,
+            description: action.payload.description,
             resvoled: false
         }
     ]}
     else if(action.type === "bugRemoved"){
+
     return state.filter(bug => bug.id !== action.payload.id)
     }
     return state
